fa1b76944b21320a86952f287dd07999f8389af2
Matthias Luebken <matthias.luebken@gmail.com>
2013-04-25 13:41:56 +0200

    sort by cards by label

--- main.js
+++ main.js
@@ -3,21 +3,46 @@ var stats = require('./lib/status'),
    moment = require('moment');
 
 var boards = [];
-boards.push({ board_name : 'Product Board', board_id: '4f71c65cdbc70d8a1a8bcf85'})
+boards.push({ board_name : 'Product Board', board_id: '4f71c65cdbc70d8a1a8bcf85'});
+
+// create a map with cards by label
+var cardsByLabel = function(cards) {
+  var cards_by_label = {};
+  cards.forEach( function( card ) {
+    var labels = [];
+    card.labels.forEach( function(l) { labels.push(l.name); });
+    labels.sort();
+    var label = labels.join();
+    if(!cards_by_label[label]) {
+      cards_by_label[label] = [];
+    } 
+    cards_by_label[label].push(card);
+  });
+  return cards_by_label;
+}
 
 var cb = function(data, callback) {
   console.log('\n\n\nSummary:')
-  data.forEach( function( card ) {
-    var comment = card.comments[0]; //TODO
-    var label = card.labels[0]; //TODO
 
-    console.log('\nTeam: ' + label.name);
-    console.log(' Update on "' + card.card_name + '". Currently in ' + card.list_name);
-    //console.log(moment(comment.date).format(' - DD.MM.'), ': ', comment.text);
-    console.log(' ' + comment.text);
+  var cards_by_label = cardsByLabel(data);
+  for (var label in cards_by_label) {
+    console.log('\n\n\nTeam: ' + label);
+    var cards = cards_by_label[label];
+    cards.forEach(function(card) {
+      var comment = card.comments[0]; //TODO
+      console.log(' - Update on "' + card.card_name + '": (Currently in ' + card.list_name + ')');
+      console.log(moment(comment.date).format('   DD.MM.:'), comment.text);
+    //console.log(' ' + comment.text);
     //console.log(' - for more info contact ' + card.id_members) //WRONG owner
-    console.log(' See also ' + card.url );
-  });
+      console.log('   Details see at ' + card.url );
+    })
+    console.log(label)
+
+  }
+  
+
+
+
 }
 
 var passed_date = new Date(2013, 4-1, 24);
